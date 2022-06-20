1e35150aa501f27f8b7af0b684a8d43fdd64ede2
Fareh Iqbal <fareh.legend@gmail.com>
2022-06-20 18:14:45 +0500

    changes

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 import java.io.*;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     private static Record [][][][][] file = new Record [10][4][10][20][3];
+    static HashMap<Integer, String> names = new HashMap<Integer, String>();
+    HashMap<Integer, Double[][]> bill = new HashMap<Integer, Double[][]>();
+
     private static Cookies cookies = new Cookies();
 
     // Write the new array to the file.
-    public static void PushData() throws IOException{
-        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(indexToMonthName(cookies.getCurrentMonth()) + ".txt")));
+    public static void PushData(String fileName) throws IOException{
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileName + ".txt")));
 
         for (int block = 0; block < 10; block++) {
             for (int subblock = 0; subblock < 4; subblock++) {
@@ -37,57 +41,14 @@ public class Main {
     
     
     // Take readings and calculate bills 
-    public static void BillCalculation() throws IOException{
-
-        double previousReading = 0, newReading = 0, monthlyBill = 0;
-
+    public static void BillCalculation() throws IOException{ // Whole Sector
         for (int block = 0; block < 10; block++) {
             for (int subblock = 0; subblock < 4; subblock++) {
                 for (int street = 0; street < 10; street++) {
                     for (int house = 0; house < 20; house++) {
                         for (int portion = 0; portion < 3; portion++) {
-                            for (int utility = 0; utility < 4 ; utility++) {
-                                for (int category = 0; category < file[block][subblock][street][house][portion].reading[utility].length; category++) {
-                                    // Getting the prevoius reading.
-                                previousReading = file[block][subblock][street][house][portion].reading[utility][category];
-                                
-                                // new readings
-                                switch(utility){
-                                    case 0:
-                                        newReading = Math.round(RandomReadings.electricity(previousReading)*100)/100d;
-                                        monthlyBill = Math.round(CalculateBill.electricity(newReading - previousReading)*100)/100d;
-                                        break;
-                                    case 1:
-                                        newReading = Math.round(RandomReadings.gas(previousReading)*100)/100d;
-                                        monthlyBill = Math.round(CalculateBill.gas(newReading - previousReading)*100)/100d;
-                                        break;
-                                    case 2:
-                                        newReading = Math.round(RandomReadings.water(previousReading)*100)/100d; 
-                                        monthlyBill =Math.round(CalculateBill.water(newReading - previousReading)*100)/100d;
-                                        break;
-                                    case 4:
-                                        switch(category){
-                                            case 0:
-                                                newReading = Math.round(RandomReadings.media.local_calls(previousReading)*100)/100d; 
-                                                monthlyBill = Math.round(CalculateBill.media.local_calls(newReading - previousReading)*100)/100d;
-                                                break;
-                                            case 1:
-                                                newReading = Math.round(RandomReadings.media.international_calls(previousReading)*100)/100d; 
-                                                monthlyBill = Math.round(CalculateBill.media.international_calls(newReading - previousReading)*100)/100d;
-                                                break;
-                                            case 2:
-                                                newReading = Math.round(RandomReadings.media.internet(previousReading)*100)/100d; 
-                                                monthlyBill = Math.round(CalculateBill.media.internet(newReading - previousReading)*100)/100d;
-                                                break;
-                                        }
-                                        
-                                }
-
-                                // Saving in Record File (Array)
-                                file[block][subblock][street][house][portion].reading[utility][category] = newReading;
-                                file[block][subblock][street][house][portion].bill[utility][category] = monthlyBill;
-                                }
-                            }
+                            // Portion
+                            BillCalculation(block, subblock, street, house, portion);
                         }
                     }
                 }
@@ -95,13 +56,105 @@ public class Main {
         }
     }
 
+    public static void BillCalculation(char block) throws IOException{ // Specific Block
+        for (int subblock = 0; subblock < 4; subblock++) {
+            for (int street = 0; street < 10; street++) {
+                for (int house = 0; house < 20; house++) {
+                    for (int portion = 0; portion < 3; portion++) {
+                        // Portion
+                        BillCalculation(block, subblock, street, house, portion);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void BillCalculation(char block, int subblock) throws IOException{ // Specific SubBlock
+        for (int street = 0; street < 10; street++) {
+            for (int house = 0; house < 20; house++) {
+                for (int portion = 0; portion < 3; portion++) {
+                    // Portion
+                    BillCalculation(block, subblock, street, house, portion);
+                }
+            }
+        }
+    }
+
+    public static void BillCalculation(char block, int subblock, int street) throws IOException{ // Specific Street
+        for (int house = 0; house < 20; house++) {
+            for (int portion = 0; portion < 3; portion++) {
+                // Portion
+                BillCalculation(block, subblock, street, house, portion);
+            }
+        }
+    }
+    
+
+    public static void BillCalculation(char block, int subblock, int street, int house) throws IOException{ // Specific House
+        for (int portion = 0; portion < 3; portion++) {
+            // Portion
+            BillCalculation(block, subblock, street, house, portion);
+        }
+    }
+    
+
+    // Take readings and calculate bills 
+    public static void BillCalculation(int block, int subblock, int street, int house, int portion) throws IOException{
+
+        double previousReading = 0, newReading = 0, monthlyBill = 0;
+
+        for (int utility = 0; utility < 4 ; utility++) {
+            for (int category = 0; category < file[block][subblock][street][house][portion].reading[utility].length; category++) {
+                // Getting the prevoius reading.
+            previousReading = file[block][subblock][street][house][portion].reading[utility][category];
+            
+            // new readings
+            switch(utility){
+                case 0:
+                    newReading = Math.round(RandomReadings.electricity(previousReading)*100)/100d;
+                    monthlyBill = Math.round(CalculateBill.electricity(newReading - previousReading)*100)/100d;
+                    break;
+                case 1:
+                    newReading = Math.round(RandomReadings.gas(previousReading)*100)/100d;
+                    monthlyBill = Math.round(CalculateBill.gas(newReading - previousReading)*100)/100d;
+                    break;
+                case 2:
+                    newReading = Math.round(RandomReadings.water(previousReading)*100)/100d; 
+                    monthlyBill =Math.round(CalculateBill.water(newReading - previousReading)*100)/100d;
+                    break;
+                case 4:
+                    switch(category){
+                        case 0:
+                            newReading = Math.round(RandomReadings.media.local_calls(previousReading)*100)/100d; 
+                            monthlyBill = Math.round(CalculateBill.media.local_calls(newReading - previousReading)*100)/100d;
+                            break;
+                        case 1:
+                            newReading = Math.round(RandomReadings.media.international_calls(previousReading)*100)/100d; 
+                            monthlyBill = Math.round(CalculateBill.media.international_calls(newReading - previousReading)*100)/100d;
+                            break;
+                        case 2:
+                            newReading = Math.round(RandomReadings.media.internet(previousReading)*100)/100d; 
+                            monthlyBill = Math.round(CalculateBill.media.internet(newReading - previousReading)*100)/100d;
+                            break;
+                    }
+                    
+            }
+
+            // Saving in Record File (Array)
+            file[block][subblock][street][house][portion].reading[utility][category] = newReading;
+            file[block][subblock][street][house][portion].bill[utility][category] = monthlyBill;
+            }
+        }
+
+    }
+
 
 
 
     // Reading data from previous month
     
-    public static void PullPreviousReadings() throws IOException{
-        BufferedReader reader = new BufferedReader(new FileReader(indexToMonthName(cookies.getPreviousMonth()) + ".txt"));
+    public static void PullPreviousReadings(String fileName) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(fileName + ".txt"));
         String record = " ";
         String[] record_array;
 
@@ -266,10 +319,10 @@ public class Main {
                             id++;
 
                             if (reqid == id){
-                                System.out.println("Id found");
+                                // System.out.println("Id found");
                                 System.out.print("The address is: ");
-                                System.out.printf("Sector %s, subblock %s, street %s, house %s, portion %s",block + 1, subblock + 1,street + 1,house + 1,portion + 1);
-                                System.out.println("Name of consumer is: ");
+                                System.out.printf("Sector %s, subblock %s, street %s, house %s, portion %s\n",block + 1, subblock + 1,street + 1,house + 1,portion + 1);
+                                System.out.print("Name of consumer is: ");
                                 PullName(reqid);
                                 
                                 
@@ -284,6 +337,21 @@ public class Main {
 
     
     }
+
+
+    public static void PrintBill() throws IOException{
+
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter your id to print bill: ");
+        // int id = input.nextInt();
+
+        getCredentials();
+    
+
+
+        // input.close();
+    }
+
     // Bill
     /**
      * 1- Bill Display:
@@ -297,14 +365,44 @@ public class Main {
      * @throws IOException
     */
 
+    public static void initializeMap(){
+        int id = 0;
+        for (int block = 0; block < 10; block++) {
+            for (int subblock = 0; subblock < 4; subblock++) {
+                for (int street = 0; street < 10; street++) {
+                    for (int house = 0; house < 20; house++) {
+                        for (int portion = 0; portion < 3; portion++) {
+                            names.put(id, file[block][subblock][street][house][portion].name);
+                            // this.bill.put(id, file[block][subblock][street][house][portion].bill);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter consumer id to be deleted: ");
-        int cid = input.nextInt();
+    //     Scanner input = new Scanner(System.in);
+    //     System.out.print("Enter consumer id to be deleted: ");
+    //     int cid = input.nextInt();
 
-        DeleteConsumer.doIt(cid);
+    //     DeleteConsumer.doIt(cid);
 
-        input.close();
+    //     input.close();'
+            PullUserIDs(); // CLass instances
+            PullName();
+            initializeMap();
+            
+
+            // PrintBill();
+            // PullPreviousReadings("Feb");
+            // BillCalculation(9,3,9,19,2);
+            // PushData("March");
+
+            System.out.println(names.get(1));
+            // System.out.println(file[9][3][9][19][1].name);
+            
+            // System.out.println(file[9][3][9][19][2].bill[0][0]);
     }
 }
--- Record.java
+++ Record.java
@@ -1,6 +1,6 @@
 public class Record {
     public int id;
-    public String name;
+    public String name = "Fareh Iqbal";
     public double[][] reading = {{0}, {0}, {0}, {0, 0, 0}};
     public double[][] bill = {{0}, {0}, {0}, {0, 0, 0}};
     
